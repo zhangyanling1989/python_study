7c7332b8ebad95168da146f49a44abe6aa261189
zhangyanling <zhangyanling123@hotmail.com>
2018-12-30 11:41:35 -0800

    重新提交

--- day14/day14.py
+++ day14/day14.py
@@ -1,42 +1,42 @@
-# def func(a,b=5,c=10):
-#     print('a is ',a , 'and b is ',b , 'and c is ',c)
-#
-# func(3,7)
-# func(25, c=24)
-# func(c=50, a = 100)
-# # print_out
-# # a is  3 and b is  7 and c is  10
-# # a is  25 and b is  5 and c is  24
-# a is  100 and b is  5 and c is  50
+def func(a,b=5,c=10):
+    print('a is ',a , 'and b is ',b , 'and c is ',c)
 
-# def maximum(x,y):
-#     if x>y:
-#         return x
-#     elif x==y:
-#         return 'The umber are equal'
-#     else:
-#         return y
-#
-# print(maximum(2,3))
-#
-# def some_function():
-#     pass
+func(3,7)
+func(25, c=24)
+func(c=50, a = 100)
+# print_out
+# a is  3 and b is  7 and c is  10
+# a is  25 and b is  5 and c is  24
+a is  100 and b is  5 and c is  50
 
-# def print_max(x,y):
-#     '''Pints the maximum if two numbers .
-#
-#     The two values must be intergers .'''
-#     #convert to integers, if possible
-#     x=int(x)
-#     y=int(y)
-#
-#     if x>y:
-#         print(x, ' is maxinum')
-#     else:
-#         print(y, ' is maxinum')
+def maximum(x,y):
+    if x>y:
+        return x
+    elif x==y:
+        return 'The umber are equal'
+    else:
+        return y
 
-# print_max(3,5)
-# print(print_max. __doc__)
+print(maximum(2,3))
+
+def some_function():
+    pass
+
+def print_max(x,y):
+    '''Pints the maximum if two numbers .
+
+    The two values must be intergers .'''
+    #convert to integers, if possible
+    x=int(x)
+    y=int(y)
+
+    if x>y:
+        print(x, ' is maxinum')
+    else:
+        print(y, ' is maxinum')
+
+print_max(3,5)
+print(print_max. __doc__)
 
 if __name__=='question.py':
     print('This program is being run by itself')
